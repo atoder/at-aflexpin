359b6e1a5e8b0be2f513a595a53415d866746be0
atoder <alexandert@gmail.com>
2014-06-30 20:44:36 -0700

    Testing config.assets.compile = true on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 	# config.assets.css_compressor = :sass
 	
 	# do not fallback to assets pipeline if a precompiled asset is missed.
-	config.assets.compile = false
+	config.assets.compile = true
 	
 	# generate digests for assets urls.
 	config.assets.digest = true
