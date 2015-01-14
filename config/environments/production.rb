a872c654a2759ce857c9a7948d7f3c93b4ed46c7
atoder <alexandert@gmail.com>
2015-01-13 18:17:04 -0800

    fixing e-mail sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "http://www.aflexpin.herokuapp.com",
+      domain: "aflexpin.herokuapp.com",
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: ENV["GMAIL_USERNAME"],
